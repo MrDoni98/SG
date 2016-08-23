6e129e7a5569ae8d4d65c987c142531a5f56be70
remote_vase <web.articlerequests@gmail.com>
2016-08-22 20:35:51 -0400

    Start

---
+++ src/ImagicalGamer/SurvivalGames/Commands/RanksCommand.php
@@ -0,0 +1,9 @@
+<?php
+namespace ImagicalGamer\SurvivalGames\Commands;
+use ImagicalGamer\SurvivalGames\BaseFiles\BaseCommand;
+use ImagicalGamer\SurvivalGames\Main;
+use pocketmine\Player;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as C;
+
+class RanksCommand extends BaseCommand{
